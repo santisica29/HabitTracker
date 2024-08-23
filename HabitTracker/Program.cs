ac4166e8a97560860cf8a2e47286ef0c1d7aee47
Santiago Sica <santisica29@gmail.com>
2024-08-23 14:34:54 -0300

    Add project files.

---
+++ HabitTracker/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS drinking_water (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+            )";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
+
+static void GetUserInput()
+{
+    Console.Clear();
+    bool closeApp = false;
+    while (closeApp == false)
+    {
+        Console.WriteLine("\n\nMAIN MENU");
+        Console.WriteLine("\nWhat would you like to do?");
+        Console.WriteLine("\nType 0 to Close Application.");
+        Console.WriteLine("Type 1 to View All Records.");
+        Console.WriteLine("Type 2 to Insert Record.");
+        Console.WriteLine("Type 3 to Delete Record.");
+        Console.WriteLine("Type 4 to Update Record.");
+        Console.WriteLine("--------------------------------------\n");
+
+        string input = Console.ReadLine();
+
+
+    }
+}

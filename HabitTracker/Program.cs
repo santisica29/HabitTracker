34748c0e0bc0c1ee652df8dce346acc04fb74738
Santiago Sica <santisica29@gmail.com>
2024-08-23 17:22:52 -0300

    created delete method

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,111 +1,195 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-string connectionString = @"Data Source=habit-Tracker.db";
+namespace HabitTrackerProgram;
 
-using (var connection = new SqliteConnection(connectionString))
+class Program
 {
-    connection.Open();
-    var tableCmd = connection.CreateCommand();
+    static string connectionString = @"Data Source=habit-Tracker.db";
 
-    tableCmd.CommandText =
-        @"CREATE TABLE IF NOT EXISTS drinking_water (
+    static void Main(string[] args)
+    {
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText =
+                @"CREATE TABLE IF NOT EXISTS drinking_water (
             Id INTEGER PRIMARY KEY AUTOINCREMENT,
             Date TEXT,
             Quantity INTEGER
             )";
 
-    tableCmd.ExecuteNonQuery();
+            tableCmd.ExecuteNonQuery();
 
-    connection.Close();
-}
+            connection.Close();
+        }
 
-GetUserInput();
+        GetUserInput();
+    }
 
-static void GetUserInput()
-{
-    Console.Clear();
-    bool closeApp = false;
-    while (closeApp == false)
+    static void GetUserInput()
     {
-        Console.WriteLine("\n\nMAIN MENU");
-        Console.WriteLine("\nWhat would you like to do?");
-        Console.WriteLine("\nType 0 to Close Application.");
-        Console.WriteLine("Type 1 to View All Records.");
-        Console.WriteLine("Type 2 to Insert Record.");
-        Console.WriteLine("Type 3 to Delete Record.");
-        Console.WriteLine("Type 4 to Update Record.");
-        Console.WriteLine("--------------------------------------\n");
-
-        string input = Console.ReadLine();
-
-        switch (input)
+        Console.Clear();
+        bool closeApp = false;
+        while (closeApp == false)
         {
-            case "0":
-                Console.WriteLine("\nGoodbye!\n");
-                closeApp = true;
-                break;
-            //case 1:
-            //    GetAllRecords();
-            //    break;
-            case "2":
-                Insert();
-                break;
-            //case 3:
-            //    Delete();
-            //    break;
-            //case 4:
-            //    Update();
-            //    break;
-            default:
-                Console.WriteLine("\nInvalid Command. PLease type a number from 0 to 4.\n");
-                break;
+            Console.WriteLine("\n\nMAIN MENU");
+            Console.WriteLine("\nWhat would you like to do?");
+            Console.WriteLine("\nType 0 to Close Application.");
+            Console.WriteLine("Type 1 to View All Records.");
+            Console.WriteLine("Type 2 to Insert Record.");
+            Console.WriteLine("Type 3 to Delete Record.");
+            Console.WriteLine("Type 4 to Update Record.");
+            Console.WriteLine("--------------------------------------\n");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "0":
+                    Console.WriteLine("\nGoodbye!\n");
+                    closeApp = true;
+                    break;
+                case "1":
+                    GetAllRecords();
+                    break;
+                case "2":
+                    Insert();
+                    break;
+                case "3":
+                    Delete();
+                    break;
+                //case 4:
+                //    Update();
+                //    break;
+                default:
+                    Console.WriteLine("\nInvalid Command. PLease type a number from 0 to 4.\n");
+                    break;
+            }
         }
     }
-}
 
-static void Insert()
-{
-    string connectionString = @"Data Source=habit-Tracker.db";
-
-    string date = GetDateInput();
-
-    int quantity = GetNumberInput("\n\nPlease insert a number of glasses or other measure fo your choise (no decimals allowed)\n\n");
-
-    using (var connection = new SqliteConnection(connectionString))
+    private static void Delete()
     {
+        Console.Clear();
+        GetAllRecords();
+
+        var recordId = GetNumberInput("\n\nPlease type the Id of the record you want to delete or type 0 to go to Main Menu \n\n");
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+            tableCmd.CommandText = $"DELETE from drinking_water WHERE Id = '{recordId}'";
+
+            int rowCount = tableCmd.ExecuteNonQuery();
+
+            if (rowCount == 0)
+            {
+                Console.WriteLine($"\n\nRecord with Id {recordId} doesn't exist. \n\n");
+                Delete();
+            }
+        }
+
+        Console.WriteLine($"\n\nRecord with Id {recordId} was deleted. \n\n");
+
+        GetUserInput();
+    }
+    internal static void GetAllRecords()
+    {
+        Console.Clear();
+
+        using var connection = new SqliteConnection(connectionString);
+
         connection.Open();
         var tableCmd = connection.CreateCommand();
-
         tableCmd.CommandText =
-           $"INSERT INTO drinking_water(date, quantity) VALUES ('{date}',{quantity})";
+           $"SELECT * FROM drinking_water";
 
-        tableCmd.ExecuteNonQuery();
+        List<DrinkingWater> tableData = new();
+
+        SqliteDataReader reader = tableCmd.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                tableData.Add(
+                new DrinkingWater
+                {
+                    Id = reader.GetInt32(0),
+                    Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
+                    Quantitiy = reader.GetInt32(2),
+                }); ;
+            }
+        }
+        else
+        {
+            Console.WriteLine("No rows found");
+        }
 
         connection.Close();
+
+        Console.WriteLine("------------------------------\n");
+        foreach (var dw in tableData)
+        {
+            Console.WriteLine($"{dw.Id} - {dw.Date.ToString("dd-MM-yyyy")} - Quantity: {dw.Quantitiy}");
+        }
+        Console.WriteLine("-------------------------------\n");
+    }
+
+    private static void Insert()
+    {
+        string date = GetDateInput();
+
+        int quantity = GetNumberInput("\n\nPlease insert a number of glasses or other measure fo your choise (no decimals allowed)\n\n");
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText =
+               $"INSERT INTO drinking_water(date, quantity) VALUES ('{date}',{quantity})";
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+
+    internal static int GetNumberInput(string str)
+    {
+        Console.WriteLine(str);
+
+        string numInput = Console.ReadLine();
+
+        if (numInput == "0") GetUserInput();
+
+        int finalInput = Convert.ToInt32(numInput);
+
+        return finalInput;
+
+    }
+
+    internal static string GetDateInput()
+    {
+        Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to main menu.");
+
+        string dateInput = Console.ReadLine();
+
+        if (dateInput == "0") GetUserInput();
+
+        return dateInput;
     }
 }
 
-static int GetNumberInput(string str)
+public class DrinkingWater
 {
-    Console.WriteLine(str);
-
-    string numInput = Console.ReadLine();
-
-    if (numInput == "0") GetUserInput();
-
-    int finalInput = Convert.ToInt32(numInput);
-
-    return finalInput;
-
-}
-
-static string GetDateInput()
-{
-    Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to main menu.");
-
-    string dateInput = Console.ReadLine();
-
-    if (dateInput == "0") GetUserInput();
-
-    return dateInput;
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int Quantitiy { get; set; }
 }
